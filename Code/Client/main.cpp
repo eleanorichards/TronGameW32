e1cb10d150b09b8830752cf29fe07847ef555e14
Ellie Richards <eleanorichards@hotmail.co.uk>
2017-03-17 14:27:47 +0000

    .

--- Code/Client/main.cpp
+++ Code/Client/main.cpp
@@ -1,7 +1,28 @@
 #include <SFML/Graphics.hpp>
 
+#include <future>
+#include <iostream>
+#include <string>
+
+#include <dinput.h>
+
+#include <SFML\Network.hpp>
+
+using TcpClient = sf::TcpSocket;
+using TcpClientPtr = std::unique_ptr<TcpClient>;
+using TcpClients = std::vector<TcpClientPtr>;
+const sf::IpAddress SERVER_IP("127.0.0.1");
+constexpr int SERVER_TCP_PORT(53000);
+
+void client();
+bool connect(TcpClient& _client);
+void input(TcpClient& _client);
+
 int main()
 {
+	std::thread clientThread(client);
+	//std::thread thread(PollInput);
+
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
@@ -21,4 +42,83 @@ int main()
 	}
 
 	return 0;
+}
+
+
+void PollInput()
+{
+	std::atomic<char> input;
+	char textInput;
+
+	while (true)
+	{
+		std::cin >> textInput;
+		input.store(textInput);
+	}
+}
+
+void client()
+{
+	TcpClient socket;
+	if (!connect(socket))
+	{
+		return;
+	}
+
+	auto handle = std::async(std::launch::async, [&]
+	{
+		// keep track of the socket status
+		sf::Socket::Status status;
+		do
+		{
+			sf::Packet packet;
+			status = socket.receive(packet);
+			if (status == sf::Socket::Done)
+			{
+				std::string message = "NULL";
+				
+				std::cin >> message;
+				
+				packet >> message;
+
+				std::cout << message << std::endl;
+			}
+		} while (status != sf::Socket::Disconnected);
+	});
+
+	return input(socket);
+}
+
+
+
+bool connect(TcpClient& _client)
+{
+	sf::Socket::Status status = _client.connect(SERVER_IP, SERVER_TCP_PORT);
+
+	if (status != sf::Socket::Done)
+	{
+		return false;
+	}
+
+	std::cout << "Connected to server: " << SERVER_IP << std::endl;
+
+	return true;
+}
+
+
+
+void input(TcpClient& _client)
+{
+	while (true)
+	{
+		auto& sender_ref = _client;
+
+		sf::Packet packet;
+		std::string message = "NULL";
+
+		std::cin >> message;
+		packet << message;
+
+		_client.send(packet);
+	}
 }
--- Code/Server/TronServerW32.cpp
+++ Code/Server/TronServerW32.cpp
@@ -1,17 +1,124 @@
-// TronServerW32.cpp : Defines the entry point for the console application.
-//
-
-#include <Server/stdafx.h>
 #include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <SFML\Network.hpp>
+
+constexpr int SERVER_TCP_PORT(53000);
+constexpr int SERVER_UDP_PORT(53001);
+
+using TcpClient = sf::TcpSocket;
+using TcpClientPtr = std::unique_ptr<TcpClient>;
+using TcpClients = std::vector<TcpClientPtr>;
+
+bool bindServerPort(sf::TcpListener&);
+void listen(sf::TcpListener&, sf::SocketSelector&, TcpClients&);
+void connect(sf::TcpListener&, sf::SocketSelector&, TcpClients&);
+void receiveMsg(TcpClients&, sf::SocketSelector&);
+void runServer();
+
+
 
 int main()
 {
-	std::cout << "Searching for life signs...";
-	while (true)
-	{
-		std::cin.get();
-	}
+	runServer();
 
-    return 0;
+	std::cout << "Server is closing" << std::endl;
+	return 0;
 }
 
+
+
+bool bindServerPort(sf::TcpListener& _listener)
+{
+	if (_listener.listen(SERVER_TCP_PORT) != sf::Socket::Done)
+	{
+		std::cout << "Could not bind server to port" << std::endl;
+		std::cout << "Port: " << SERVER_TCP_PORT << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+
+
+void listen(sf::TcpListener& _tcp_listener, sf::SocketSelector& _socket_selector, TcpClients& _tcp_clients)
+{
+	while (true)
+	{
+		if (_socket_selector.wait())
+		{
+			if (_socket_selector.isReady(_tcp_listener))
+			{
+				connect(_tcp_listener, _socket_selector, _tcp_clients);
+			}
+			else
+			{
+				receiveMsg(_tcp_clients, _socket_selector);
+			}
+		}
+
+	}
+}
+
+
+
+void connect(sf::TcpListener& _tcp_listener, sf::SocketSelector& _socket_selector, TcpClients& _tcp_clients)
+{
+	auto client_ptr = std::make_unique<sf::TcpSocket>();
+	auto& client_ref = *client_ptr;
+
+	if (_tcp_listener.accept(client_ref) == sf::Socket::Done)
+	{
+		std::cout << "Client has connected" << std::endl;
+
+		_socket_selector.add(client_ref);
+		_tcp_clients.push_back(std::move(client_ptr));
+	}
+}
+
+
+
+void receiveMsg(TcpClients& _tcp_clients, sf::SocketSelector& _socket_selector)
+{
+	for (auto& client : _tcp_clients)
+	{
+		auto& sender_ref = *client;
+		if (_socket_selector.isReady(sender_ref))
+		{
+			sf::Packet packet;
+			client->receive(packet);
+
+			std::string message = "NULL";
+			packet >> message;
+
+			for (auto& connected_client : _tcp_clients)
+			{
+				//Spams when disconnecting
+				connected_client->send(packet);
+			}
+			std::cout << message;
+		}
+	}
+
+}
+
+
+
+void runServer()
+{
+	sf::TcpListener tcp_listener;
+
+	if (!bindServerPort(tcp_listener))
+	{
+		return;
+	}
+
+	sf::SocketSelector socket_selector;
+	socket_selector.add(tcp_listener);
+
+	TcpClients tcp_clients;
+	return listen(tcp_listener, socket_selector, tcp_clients);
+}
